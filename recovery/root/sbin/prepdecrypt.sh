5617f3e2b08e90ab002efb36590f4eb134073900
Willi Ye <williye97@gmail.com>
2020-07-07 21:35:33 +0200

    Use A705FNXXU5BTF1 blobs
    Change-Id: Ibe68c8dd30cc8de4c0cc3d85ca4ac9395822294d

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -25,52 +25,6 @@ else
 	setprop ro.build.version.security_patch "$patchlevel"
 fi
 
-relink /v/bin/hw/android.hardware.gatekeeper@1.0-service
-if [ -f /v/bin/hw/android.hardware.keymaster@4.0-service.samsung ]; then
-	relink /v/bin/hw/android.hardware.keymaster@4.0-service.samsung
-	mv /sbin/android.hardware.keymaster@4.0-service.samsung /sbin/android.hardware.keymaster@4.0-service
-else
-	relink /v/bin/hw/android.hardware.keymaster@4.0-service
-fi
-relink /v/bin/qseecomd
-
-mkdir -p /vendor/lib64/hw
-cp /v/lib64/hw/android.hardware.gatekeeper@1.0-impl.so /vendor/lib64/hw/
-cp /v/lib64/hw/gatekeeper.mdfpp.so /vendor/lib64/hw/
-cp /v/lib64/hw /vendor/lib64/
-cp /v/lib64/libdiag.so /vendor/lib64/
-cp /v/lib64/libdrmfs.so /vendor/lib64/
-cp /v/lib64/libdrmtime.so /vendor/lib64/
-cp /v/lib64/libdsutils.so /vendor/lib64/
-cp /v/lib64/libGPreqcancel.so /vendor/lib64/
-cp /v/lib64/libGPreqcancel_svc.so /vendor/lib64/
-cp /v/lib64/libidl.so /vendor/lib64/
-cp /v/lib64/libkeymaster4.so /vendor/lib64/
-cp /v/lib64/libkeymasterdeviceutils.so /vendor/lib64/
-cp /v/lib64/libkeymaster_helper_vendor.so /vendor/lib64/
-cp /v/lib64/libkeymasterprovision.so /vendor/lib64/
-cp /v/lib64/libkeymasterutils.so /vendor/lib64/
-cp /v/lib64/libmdmdetect.so /vendor/lib64/
-cp /v/lib64/libqdutils.so /vendor/lib64/
-cp /v/lib64/libqisl.so /vendor/lib64/
-cp /v/lib64/libqmi_cci.so /vendor/lib64/
-cp /v/lib64/libqmi_client_qmux.so /vendor/lib64/
-cp /v/lib64/libqmi_common_so.so /vendor/lib64/
-cp /v/lib64/libqmi_encdec.so /vendor/lib64/
-cp /v/lib64/libqmiservices.so /vendor/lib64/
-cp /v/lib64/libQSEEComAPI.so /vendor/lib64/
-cp /v/lib64/libqservice.so /vendor/lib64/
-cp /v/lib64/libqtikeymaster4.so /vendor/lib64/
-cp /v/lib64/librpmb.so /vendor/lib64/
-cp /v/lib64/libSecureUILib.so /vendor/lib64/
-cp /v/lib64/libsecureui.so /vendor/lib64/
-cp /v/lib64/libsecureui_svcsock.so /vendor/lib64/
-cp /v/lib64/libskeymaster4device.so /vendor/lib64/
-cp /v/lib64/libssd.so /vendor/lib64/
-cp /v/lib64/libStDrvInt.so /vendor/lib64/
-cp /v/lib64/libtime_genoff.so /vendor/lib64/
-cp /v/lib64/vendor.qti.hardware.tui_comm@1.0.so /vendor/lib64/
-
 ln -s /firmware /vendor/firmware_mnt
 
 if [ -f /s/system/framework/samsungkeystoreutils.jar ]; then
